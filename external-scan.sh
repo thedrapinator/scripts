ba713db025a0ce3b00c93e0436ebf5f5957bf6c1
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-13 10:00:41 -0400

    Update external-scan.sh

--- external-scan.sh
+++ external-scan.sh
@@ -16,7 +16,7 @@ cidr=`sed -z 's/\n/ -cidr /g' $companypath/inscope.txt | sed 's/.......$//g'`
 mkdir -p $companypath
 
 echo "ENTER/VERIFY IN SCOPE IP ADDRESSES ONE ON EACH LINE IN CIDR NOTATION!!! Opening file in gedit please wait....."
-sleep 5
+sleep 3
 gedit $companypath/inscope.txt
 
 # if inscope does not exist then exit
@@ -33,21 +33,23 @@ fi
 END
 #########################################
 
-### nmap scan ##                               ######### Replace with autorecon ##########
-#mkdir -p $companypath/$companyname/nmap
-#nmap -vv -sV -O -iL inscope.txt -oA $companypath/nmap/$companyname
+### nmap scan ##
+mkdir -p $companypath/$companyname/nmap
+nmap -vv -sV -O -iL inscope.txt -oA $companypath/nmap/$companyname
 
-## DNS zone transfer attempt??
+##Convert nmap scan to CSV for spreadsheet
+python3 ~/scripts/xml2csv.py -f $companypath/nmap/$companyname.xml -csv $companypath/nmap/$companyname.csv
+
+# eyewitness
+cd $companypath/
+eyewitness -x $companypath/nmap/$companyname.xml --no-prompt --delay 5 -d $companypath/eyewitness      
+
+## DNS zone transfer attempt ## AUTORECON DOES THIS
 #cat $companyname.gnmap| grep 53/open | cut -d " " -f2 > $companypath/nmap/dns_servers.txt
 #nmap -p53 -sV -v --script=dns-zone-transfer.nse -iL $companypath/nmap/dns_servers.txt -oA $companypath/nmap/dns_zone_results
 
+### AUTORECON ###
 echo "STARTING AUTORECON!!!"
 mkdir -p $companypath/autorecon
 cd $companypath/autorecon
 autorecon -t $companypath/inscope.txt --only-scans-dir -o $companypath/autorecon
-
-# eyewitness (run at end because of prompt)
-cd $companypath/
-#### Fix to read autorecon results #####
-#eyewitness -x $companypath/nmap/$companyname.xml --delay 5 -d $companypath/eyewitness     
-
