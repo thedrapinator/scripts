393d0710268d6207b631eb46d0ff691ef72e1b66
sirchsec <85844648+sirchsec@users.noreply.github.com>
2022-04-25 14:19:42 -0400

    Update install-vm-tools.sh

--- install-vm-tools.sh
+++ install-vm-tools.sh
@@ -4,14 +4,14 @@
 
 cd /opt
 sudo apt update
-sudo apt install -y bloodhound eyewitness chromium gedit enum4linux
+sudo apt install -y bloodhound eyewitness chromium gedit enum4linux ldapscripts
 
 #eyewitness install
 cd /opt
 sudo git clone https://github.com/FortyNorthSecurity/EyeWitness.git
 sudo /opt/EyeWitness/Python/setup/setup.sh
 #run
-/opt/EyeWitness/Python/EyeWitness.py -x nmap.xml --no-prompt --delay 10 -d eyewitness
+#/opt/EyeWitness/Python/EyeWitness.py -x nmap.xml --no-prompt --delay 10 -d eyewitness
 
 #xmltocsv
 sudo git clone https://github.com/laconicwolf/Nmap-Scan-to-CSV.git
