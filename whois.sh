53171073aaad072c829f52f4f1bae0a82267332e
sirchsec <85844648+sirchsec@users.noreply.github.com>
2022-04-28 16:00:54 -0400

    Update whois.sh

--- whois.sh
+++ whois.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
 
-#Usage whois.sh <domain-name>
+#Create file with IP's in scope named inscope.txt
+#Run script with no arguments
+#Run script with company names and script will sort in scope IP to folders
 
-touch whois.txt
-while read ip; do echo $ip; whois $ip | grep -i $1; done < inscope.txt | tee whois.txt
+threads=10
+
+function maxrun {
+   while [ $(jobs | wc -l) -ge $threads ]
+   do
+      sleep 1
+   done
+}
+
+datapath=./whoisdata
+mkdir -p $datapath
+
+if [ $# -eq 0 ]
+  then
+    while read ip
+    do 
+      maxrun; echo "$ip RUNNING"; whois $ip > $datapath/$ip.txt &
+    done < inscope.txt
+    wait
+  else
+for f in $datapath/*.txt
+  do
+    #echo "Filename is $f"
+    IP=`echo $f | rev | cut -d "/" -f1 | cut -d "." -f2,3,4,5 | rev`
+    count=`cat $f | grep -i "$1" | wc -l`
+    echo "$IP $1 $count"
+    if [ $count -ge 1 ]
+    then
+      echo "MOVING VALIDATED IP $IP to $1 folder..."
+      mkdir -p "$datapath/$1"
+      mv $f "$datapath/$1"
+    fi
+  done
+  
+  #Count remaining
+  remaining=`ls $datapath/*.txt | wc -l`
+  echo "$remaining RECORDS REMAINING!"
+fi
