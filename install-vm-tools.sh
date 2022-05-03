7b67bb6ee921055f20b7cc84c98c75d21ace70a5
sirchsec <85844648+sirchsec@users.noreply.github.com>
2022-05-03 10:16:12 -0400

    Update install-vm-tools.sh

--- install-vm-tools.sh
+++ install-vm-tools.sh
@@ -7,15 +7,16 @@ sudo apt update
 sudo apt install -y bloodhound eyewitness chromium gedit enum4linux ldapscripts jq
 
 #eyewitness install
-cd /opt
 sudo git clone https://github.com/FortyNorthSecurity/EyeWitness.git
 sudo /opt/EyeWitness/Python/setup/setup.sh
-#run
-#/opt/EyeWitness/Python/EyeWitness.py -x nmap.xml --no-prompt --delay 10 -d eyewitness
+#run with: /opt/EyeWitness/Python/EyeWitness.py -x nmap.xml --no-prompt --delay 10 -d eyewitness
+
+#nmap-grep
+sudo git clone https://github.com/sirchsec/nmap-grep.git
 
 #xmltocsv
 sudo git clone https://github.com/laconicwolf/Nmap-Scan-to-CSV.git
 
-echo "COPY OVER SSH CONFIG AND PROXYCHAINS CONFIG"
+echo "COPY OVER SSH CONFIG AND CHANGE PROXYCHAINS CONFIG"
 echo "MANUAL INSTALL FOXYPROXY AND WAPALIZER"
 echo "CHANGE TRANSPARENCY IN VM"
