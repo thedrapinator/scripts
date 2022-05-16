2d5620787472d4439b2f940ad68abad9f59ad0df
sirchsec <85844648+sirchsec@users.noreply.github.com>
2022-05-16 11:39:56 -0400

    Update install-vm-tools.sh

--- install-vm-tools.sh
+++ install-vm-tools.sh
@@ -2,6 +2,12 @@
 
 #Kali-install script
 
+install=~/tools
+
+#make folder if it does not exist
+mkdir -p $install
+
+
 cd /opt
 sudo apt update
 sudo apt reinstall libwacom-common -y
@@ -9,28 +15,26 @@ sudo apt upgrade -y
 sudo apt install -y bloodhound eyewitness chromium gedit enum4linux ldapscripts jq
 
 #eyewitness install
-sudo git clone https://github.com/FortyNorthSecurity/EyeWitness.git
-sudo /opt/EyeWitness/Python/setup/setup.sh
+git clone https://github.com/FortyNorthSecurity/EyeWitness.git $install/Eyewitness
+$install/EyeWitness/Python/setup/setup.sh
 #run with: /opt/EyeWitness/Python/EyeWitness.py -x nmap.xml --no-prompt --delay 10 -d eyewitness
 
 #Interlace install for multi threading commands
-sudo git clone https://github.com/codingo/Interlace /opt/Interlace
-sudo python3 /opt/Interlace/setup.py install
+git clone https://github.com/codingo/Interlace  $install/Interlace
+sudo python3  $install/Interlace/setup.py install
 
 #Discover OSINT Tool
-sudo git clone https://github.com/leebaird/discover /opt/discover/
+git clone https://github.com/leebaird/discover $install/discover/
 #All scripts must be ran from this location.
-sudo chmod +x /opt/discover/update.sh
-cd /opt/discover
+sudo chmod +x  $install/discover/update.sh
+cd  $install/discover
 sudo ./update.sh
 
 #nmap-grep
-cd /opt
-sudo git clone https://github.com/sirchsec/nmap-grep.git
+git clone https://github.com/sirchsec/nmap-grep.git $install/nmap-grep
 
 #xmltocsv
-cd /opt
-sudo git clone https://github.com/laconicwolf/Nmap-Scan-to-CSV.git
+git clone https://github.com/laconicwolf/Nmap-Scan-to-CSV.git  $install/Nmap-Scan-to-CSV
 
 echo "COPY OVER SSH CONFIG AND CHANGE PROXYCHAINS CONFIG"
 echo "MANUAL INSTALL FOXYPROXY AND WAPALIZER"
