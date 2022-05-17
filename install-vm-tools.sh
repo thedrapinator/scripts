bf804f899aa699b070043b29e28db85fc2926fdd
sirchsec <85844648+sirchsec@users.noreply.github.com>
2022-05-17 12:19:39 -0400

    Update install-vm-tools.sh

--- install-vm-tools.sh
+++ install-vm-tools.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
 
-#Kali-install script
+#Sudo command to prompt for sudo privs
+sudo echo 'SUDO PASSWORD CACHED'
 
-install=~/tools
+user='whoami'
+tools=/home/$user/tools
+scripts=/home/$user/scripts
 
 #make folder if it does not exist
-mkdir -p $install
+mkdir -p $tools
 
-#sudo apt update
-#sudo apt reinstall libwacom-common -y
-#sudo apt upgrade -y
+sudo apt update
+sudo apt reinstall libwacom-common -y
+sudo apt upgrade -y
 sudo apt install -y bloodhound chromium gedit enum4linux ldapscripts jq
 
 #eyewitness install
-git clone https://github.com/FortyNorthSecurity/EyeWitness.git $install/Eyewitness
-sudo $install/EyeWitness/Python/setup/setup.sh
+git clone https://github.com/FortyNorthSecurity/EyeWitness.git $tools/Eyewitness
+sudo $tools/EyeWitness/Python/setup/setup.sh
 #run with: /opt/EyeWitness/Python/EyeWitness.py -x nmap.xml --no-prompt --delay 10 -d eyewitness
 
 #Interlace install for multi threading commands
-git clone https://github.com/codingo/Interlace  $install/Interlace
-sudo python3 $install/Interlace/setup.py install
+#git clone https://github.com/codingo/Interlace  $toolsInterlace
+#sudo python3 $install/Interlace/setup.py install
 
 #Discover OSINT Tool
-git clone https://github.com/leebaird/discover $install/discover/
+git clone https://github.com/leebaird/discover $tools/discover/
 #All scripts must be ran from this location.
-sudo chmod +x  $install/discover/update.sh
-cd  $install/discover
+sudo chmod +x  $tools/discover/update.sh
+cd  $tools/discover
 sudo ./update.sh
 
 #nmap-grep
-git clone https://github.com/sirchsec/nmap-grep.git $install/nmap-grep
+git clone https://github.com/sirchsec/nmap-grep.git $tools/nmap-grep
 
 #xmltocsv
-git clone https://github.com/laconicwolf/Nmap-Scan-to-CSV.git  $install/Nmap-Scan-to-CSV
+git clone https://github.com/laconicwolf/Nmap-Scan-to-CSV.git  $tools/Nmap-Scan-to-CSV
 
 echo "COPY OVER SSH CONFIG AND CHANGE PROXYCHAINS CONFIG"
 echo "MANUAL INSTALL FOXYPROXY AND WAPALIZER"
-echo "CHANGE TRANSPARENCY IN VM"
+echo "CHANGE TRANSPARENCY IN TERMINAL"
