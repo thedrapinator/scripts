348e96c3010b25aed4e3caac9d051d5acdecfd82
sirchsec <85844648+sirchsec@users.noreply.github.com>
2022-05-20 10:21:05 -0400

    Update osint.sh

--- osint.sh
+++ osint.sh
@@ -64,6 +64,7 @@ firefox "https://phonebook.cz/"&
 #firefox "https://hunter.io/"&
 
 #Combine email lists, clean, make possible users list
+echo "==== LAUNCHING GEDIT PLEASE ADD EMAILS FROM PHONEBOOK.CZ TO FILE ===="
 gedit $companypath/emails_combined.txt
 cat $companypath/emails_combined.txt | cut -d "@" -f1 | grep -v "\." | sort -u > $companypath/possible_users_tmp.txt
 cat $companypath/emails_combined.txt | cut -d "@" -f1 | grep "\." | sed -r 's/(.)\S*\.(.*)/& \L\1\2/' | cut -d " " -f2 | sort -u >> $companypath/possible_users_tmp.txt
