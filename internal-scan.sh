a246d8b0a0ae79a888d419228f5db988b7f4d535
sirchsec <85844648+sirchsec@users.noreply.github.com>
2022-06-16 09:18:07 -0400

    Update internal-scan.sh

--- internal-scan.sh
+++ internal-scan.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-#Sudo command to prompt for sudo privs
-tools=/root/tools
-scripts=/root/scripts
+tools=~/tools
+scripts=~/scripts
 
-### SET VARIABLES ###
-companypath=/root
+companypath=~/projects
 echo "Files stored in $companypath"
+
+#make folder if it does not exist
+mkdir -p $companypath
 
 echo "ENTER/VERIFY IN SCOPE IP ADDRESSES ONE ON EACH LINE IN CIDR NOTATION!!! Opening file in gedit please wait....."
 sleep 1
-nano $companypath/inscope.txt
+gedit $companypath/inscope.txt
 
 # if inscope does not exist then exit
 if [ ! -f $companypath/inscope.txt ]
@@ -34,31 +35,28 @@ nmap -v -sV -iL $companypath/inscope.txt -oA $companypath/nmap/nmap
 python3 $scripts/xml2csv.py -f $companypath/nmap/nmap.xml -csv $companypath/nmap/nmap.csv
 #python3 /opt/Nmap-Scan-to-CSV/nmap_xml_parser.py -f $companypath/nmap/nmap.xml -csv $companypath/nmap/nmap.csv
 
-# eyewitness
-mkdir -p $companypath/eyewitness
-cd $companypath/eyewitness
-#COPY FROM REMOTE HOST FIRST
-#$tools/Eyewitness/Python/EyeWitness.py --proxy-ip 127.0.0.1 --proxy-port 8810 --proxy-type socks5 -x $companypath/nmap/nmap.xml --no-prompt --delay 10 -d $companypath/eyewitness
+# Aquatone
+mkdir -p $companypath/aquatone
+cd $companypath/aquatone
+cat $companypath/nmap/nmap.xml | $tools/aquatone -nmap -out $companypath/aquatone
+#$tools/aquatone -nmap $companypath/nmap/nmap.xml -ports xlarge -out $companypath/aquatone 
 
 # nmap-grep
 $tools/nmap-grep/nmap-grep.sh $companypath/nmap/nmap.gnmap --out-dir $companypath/nmap/parsed --no-summary
 
+### Add Metasploit Scripts ###
+#copy metasploit rc file
+cd $companypath/nmap/parsed
+msfconsole -r $scripts/metasploit.rc
+
 #Make results folder
 mkdir -p $companypath/nmap/results
-
-#GET INTERLACE WORKING
-
-#Parallel example
-#cat websites.txt | parallel -j 16 dirb {} -f -o websites.dirb
-#parallel -a list.txt -j 10 echo {} ">" {}.log
-#parallel -a test -j 2 echo {}";" sleep 3
 
 #DNSrecon
 echo "RUNNING DNS RECON"
 mkdir -p $companypath/nmap/results/dnsrecon
 cd $companypath/nmap/results/dnsrecon
 parallel -a $companypath/nmap/parsed/dns-tcp-hosts.txt --progress -j 10 "dnsrecon -d {} -t axfr > {=s/\///g=}"
-
 
 #SSLScan
 echo "RUNNING SSL SCAN"
@@ -68,6 +66,12 @@ cd $companypath/nmap/results/sslscan
 #while read -r line; do sslscan $line | tee $companypath/nmap/results/sslscan/`echo $line | sed 's/\///g'`; done < $companypath/nmap/parsed/https-hosts.txt
 parallel -a $companypath/nmap/parsed/https-hosts.txt --progress -j 10 "sslscan {} > {=s/\///g=}"
 
+#SSL Results
+cd $companypath/nmap/results/sslscan
+grep "vulnerable" * | grep -v "not" > vulnerable.txt #NEED TO CUT OUT IP IF RESULTS
+grep "enabled" * | grep "TLSv1.0" | cut -d ":" -f1 > tls10.txt
+grep "enabled" * | grep "TLSv1.1" | cut -d ":" -f1 > tls11.txt
+
 #nikto
 echo "RUNNING NIKTO"
 mkdir -p $companypath/nmap/results/nikto
@@ -75,7 +79,19 @@ cd $companypath/nmap/results/nikto
 #while read -r line; do nikto -h $line; done < $companypath/nmap/parsed/web-urls.txt | tee $companypath/nmap/results/nikto.txt
 #while read -r line; do proxychains -q nikto -h $line -maxtime 1h | tee $companypath/nmap/results/nikto/`echo $line | sed 's/\///g'`; done < $companypath/nmap/parsed/web-urls.txt
 #parallel -a $companypath/nmap/parsed/web-urls.txt --progress -j 10 proxychains -q nikto -h {} -maxtime 1h -output . -Format txt
-parallel -a $companypath/nmap/parsed/web-urls.txt --progress -j 10 "nikto -h {} -maxtime 1h > {=s/\///g=}"
+parallel -a $companypath/nmap/parsed/web-urls.txt --progress -j 10 "proxychains -q nikto -h {} -maxtime 1h > {=s/\///g=}"
+
+#Nikto Grep Vulns
+cd $companypath/nmap/results/nikto
+grep -i 'real ip' http* > ip-in-header.txt  #Internal IP in Header
+grep -i 'ip address found' http* >> ip-in-header.txt
+grep -i 'outdated' http* > outdated-software.txt
+grep -i 'interesting' http* > interesting.txt
+grep -i 'indexing' http* >> interesting.txt
+grep -i 'OSVBD' http* > osvdb.log
+grep -i 'RFC' http* > rfc.log
+grep -i 'vulnerable' http* > vulnerable.log
+
 
 #ffuf
 mkdir -p $companypath/nmap/results/ffuf
@@ -86,7 +102,7 @@ cd $companypath/nmap/results/ffuf
 #interlace -tL <domain list> -c "ffuf -u _target_ -w /usr/share/wordlists/dirb/common.txt -se -sf -mc all -fc 300,301,302,303,500,400,404 | tee ffuf/$url.txt
 #interlace -tL $companypath/nmap/parsed/web-urls.txt -c "ffuf -u _target_ -w /usr/share/wordlists/dirb/common.txt -se -sf -mc all -fc 300,301,302,303,500,400,404 | tee ffuf/$url.txt
 #while read -r line; do proxychains -q ffuf -w /usr/share/wordlists/dirb/common.txt -u $line''FUZZ -maxtime-job 3600 -se -sf -mc all -fc 300,301,302,303,500,400,404 | tee $companypath/nmap/results/ffuf/`echo $line | sed 's/\///g'`; done < $companypath/nmap/parsed/web-urls.txt
-parallel -a $companypath/nmap/parsed/web-urls.txt --progress -j 10 "ffuf -w /usr/share/wordlists/dirb/common.txt -u {}FUZZ -maxtime-job 3600 -noninteractive -se -sf -mc all -fc 300,301,302,303,500,400,404 > {=s/\///g=}"
+parallel -a $companypath/nmap/parsed/web-urls.txt --progress -j 10 "proxychains -q ffuf -w /usr/share/wordlists/dirb/common.txt -u {}FUZZ -maxtime-job 3600 -noninteractive -se -sf -mc all -fc 300,301,302,303,500,400,404 > {=s/\///g=}"
 
 
 echo "SCRIPT COMPLETED!!!"
