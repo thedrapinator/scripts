7c0bbf351b05c3e8b4a43eef82100a9138df2d7d
sirchsec <85844648+sirchsec@users.noreply.github.com>
2022-06-22 11:26:04 -0400

    Update combine-ntlm.sh

--- combine-ntlm.sh
+++ combine-ntlm.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#Arg1 is ?????
-#Arg 2 is ?????
+# Usage: combine-ntlm.sh ntds.dit cracked.txt
 
 while read -r line; do grep `echo $line | cut -d ":" -f1` $1; echo $line|cut -d ":" -f2  ; done < $2
