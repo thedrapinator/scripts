9950cca3a0ef7853c5d7188108b39d8a44607457
sirchsec <85844648+sirchsec@users.noreply.github.com>
2022-06-30 10:02:58 -0400

    Update combine-ntlm.sh

--- combine-ntlm.sh
+++ combine-ntlm.sh
@@ -2,6 +2,9 @@
 
 # Usage: combine-ntlm.sh ntds.dit cracked.txt
 
-#while read -r line; do grep `echo $line | cut -d ":" -f1` $1; echo $line|cut -d ":" -f2  ; done < $2
+#Get Uerlist of cracked hashes
+while read -r line; do grep `echo $line | cut -d ":" -f1` $1; echo $line|cut -d ":" -f2; done < $2 | grep ::: | cut -d '\' -f2 | cut -d : -f1 > CRACKED-USERLIST.txt
 
-while read -r line; do grep `echo $line | cut -d ":" -f4` $2; echo $line|cut -d ":" -f1,4 | cut -d " " -f5; done < $1
+#Password matching for pipal analysis
+#while read -r line; do grep `echo $line | cut -d ":" -f4` $2; echo $line|cut -d ":" -f1,4 | cut -d " " -f5; done < $1
+while read -r line; do grep `echo -n $line | cut -d ":" -f4` $2; echo -n $line|cut -d ":" -f1,4| cut -d " " -f5 ; done < $1 | grep -v '\\' | cut -d : -f2 > PIPAL.txt
