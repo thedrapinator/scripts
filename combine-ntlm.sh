a7bc0f60302f65b5bbc1f878028d11aff84434f4
sirchsec <85844648+sirchsec@users.noreply.github.com>
2022-06-30 10:44:20 -0400

    Update combine-ntlm.sh

--- combine-ntlm.sh
+++ combine-ntlm.sh
@@ -2,14 +2,22 @@
 
 # Usage: combine-ntlm.sh ntds.dit cracked.txt
 
-
-cat $1 | grep ::: | grep -v '\$' > tmp.ntds.users.txt
+#cat $1 | grep ::: | grep -v '\$' > tmp.ntds.users.txt
 
 #Get Userlist of cracked hashes
-while read -r line; do grep `echo $line | cut -d ":" -f1` tmp.ntds.users.txt; echo $line|cut -d ":" -f2; done < $2 | grep ::: | cut -d '\' -f2 | cut -d : -f1 > CRACKED-USERLIST.txt
+#while read -r line; do grep `echo $line | cut -d ":" -f1` tmp.ntds.users.txt; echo $line|cut -d ":" -f2; done < $2 | grep ::: | cut -d '\' -f2 | cut -d : -f1 > zCRACKED-USERLIST.txt
 
 #Password matching for pipal analysis
 #while read -r line; do grep `echo $line | cut -d ":" -f4` $2; echo $line|cut -d ":" -f1,4 | cut -d " " -f5; done < tmp.ntds.users.txt
-while read -r line; do grep `echo -n $line | cut -d ":" -f4` $2; echo -n $line|cut -d ":" -f1,4| cut -d " " -f5 ; done < tmp.ntds.users.txt | grep -v '\\' | cut -d : -f2 > PIPAL.txt
+#while read -r line; do grep `echo -n $line | cut -d ":" -f4` $2; echo -n $line|cut -d ":" -f1,4| cut -d " " -f5 ; done < tmp.ntds.users.txt | grep -v '\\' | cut -d : -f2 > zPIPAL.txt
 
-rm tmp.ntds.users.txt
+#rm tmp.ntds.users.txt
+
+cat $2 | cut -d : -f2 > tmp.pass.txt
+cat $1 | grep ::: | cut -d ' ' -f26  > tmp.ntds.txt
+john tmp.ntds.txt --wordlist=tmp.pass.txt --format=NT
+john tmp.ntds.txt --format=NT --show | cut -d : -f1,2 > COMBINED.txt
+
+cat COMBINED.txt | cut -d : -f1 > COMBINED-USERS.txt
+cat COMBINED.txt | cut -d : -f2 > COMBINED-PASS.txt
+pipal COMBINED-PASS.txt
