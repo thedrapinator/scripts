5c2256e3cbc628211a940edf10423171fb367d7e
sirchsec <85844648+sirchsec@users.noreply.github.com>
2022-06-30 09:44:57 -0400

    Update combine-ntlm.sh

--- combine-ntlm.sh
+++ combine-ntlm.sh
@@ -2,4 +2,6 @@
 
 # Usage: combine-ntlm.sh ntds.dit cracked.txt
 
-while read -r line; do grep `echo $line | cut -d ":" -f1` $1; echo $line|cut -d ":" -f2  ; done < $2
+#while read -r line; do grep `echo $line | cut -d ":" -f1` $1; echo $line|cut -d ":" -f2  ; done < $2
+
+while read -r line; do grep `echo $line | cut -d ":" -f4` $2; echo $line|cut -d ":" -f1,4  ; done < $1
