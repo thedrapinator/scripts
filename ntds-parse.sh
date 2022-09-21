aa53afb1d6862d8d5e8e3e06eee18b05c43b5e29
sirchsec <85844648+sirchsec@users.noreply.github.com>
2022-09-21 12:00:00 -0400

    Update ntds-parse.sh

--- ntds-parse.sh
+++ ntds-parse.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# Argument 1 is ntds file
-
-#Grep users only
-cat $1 | grep ::: | grep -v '\$' > log.ntds.users
-
-#LM Hashes enabled
-cat log.ntds.users | grep -av 'aad3b435b51404eeaad3b435b51404ee' > log.lm.hash.enabled
-
-#Blank Password
-cat log.ntds.users | grep -av '31d6cfe0d16ae931b73c59d7e0c089c0' > log.no.pass
+#impacket-secretsdump corp.yamww.com/rapid7:'Rapid7!Rapid7!'@10.8.22.39 -user-status -outputfile log.NTDS.secretsdump
+cat *.ntds | grep Enabled | grep -v '\$' | grep ::: | rev | cut -d ' ' -f2,3,4,5,6,7,8,9 | rev > ntds.all
+echo "`wc -l ntds.all` user hashes"
 
 #NT Hashes for hashtopolis
-cat log.ntds.users | cut -d : -f4 | sort -u > log.nt.hashes
+cat ntds.all | cut -d : -f4 | sort -u > ntds.nt.hashtopolis
+echo "`wc -l ntds.nt.hashtopolis` unique user hashes"
+
+#LM Hashes enabled
+cat ntds.all | grep -av 'aad3b435b51404eeaad3b435b51404ee' > ntds.lm.hash.enabled
+echo "`wc -l ntds.lm.hash.enabled` accounts with LM hashes enabled"
+
+#Blank Password
+cat ntds.all | grep -av '31d6cfe0d16ae931b73c59d7e0c089c0' > ntds.no.pass
+echo "`wc -l ntds.no.pass` accounts with blank passwords"
+
+cat ntds.all | grep -av '8846f7eaee8fb117ad06bdd830b7586c' > ntds.password.as.password
+echo "`wc -l ntds.password.as.password` accounts with password as password"
