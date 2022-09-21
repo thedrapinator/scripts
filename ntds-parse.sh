cddbde27c75081743fb8f908a61741865406d73c
sirchsec <85844648+sirchsec@users.noreply.github.com>
2022-09-21 13:23:26 -0400

    Update ntds-parse.sh

--- ntds-parse.sh
+++ ntds-parse.sh
@@ -9,13 +9,13 @@ cat ntds.all | cut -d : -f4 | sort -u > ntds.nt.hashtopolis
 echo "`cat ntds.nt.hashtopolis | wc -l` unique user password hashes"
 
 #LM Hashes enabled
-cat ntds.all | grep -av 'aad3b435b51404eeaad3b435b51404ee' > ntds.lm.hash.enabled
+cat ntds.all | grep -av 'aad3b435b51404eeaad3b435b51404ee' | cut -d : -f1 > ntds.lm.hash.enabled
 echo "`cat ntds.lm.hash.enabled | wc -l` accounts with LM hashes enabled"
 
 #Blank Password
-cat ntds.all | grep -a '31d6cfe0d16ae931b73c59d7e0c089c0' > ntds.no.pass
+cat ntds.all | grep -a '31d6cfe0d16ae931b73c59d7e0c089c0' | cut -d : -f1 > ntds.no.pass
 echo "`cat ntds.no.pass | wc -l` accounts with blank passwords"
 
-cat ntds.all | grep -a '8846f7eaee8fb117ad06bdd830b7586c' > ntds.password.as.password
+cat ntds.all | grep -a '8846f7eaee8fb117ad06bdd830b7586c' | cut -d : -f1 > ntds.password.as.password
 echo "`cat ntds.password.as.password | wc -l` accounts with password as password"
 
