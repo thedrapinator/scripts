023a51c274b88db73f82c89dae61643497aa2a0a
thedrapinator <85844648+thedrapinator@users.noreply.github.com>
2023-12-26 11:36:17 -0500

    Update external-scan.sh
    adding no interact to nuclei

--- external-scan.sh
+++ external-scan.sh
@@ -114,7 +114,7 @@ grep "Issuer" * > certificates.txt
 echo "RUNNING NUCLEI"
 mkdir -p $companypath/nmap/results/nuclei
 cd $companypath/nmap/results/nuclei
-parallel -a $companypath/nmap/parsed/web-urls.txt --progress -j 10 "nuclei -u {} > {=s/\///g=}"
+parallel -a $companypath/nmap/parsed/web-urls.txt --progress -j 10 "nuclei -ni -u {} > {=s/\///g=}"
 
 #nikto
 echo "RUNNING NIKTO"
