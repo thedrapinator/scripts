66e7f443a44e653b1cc9a6ebe3909c5e13b64d62
thedrapinator <85844648+thedrapinator@users.noreply.github.com>
2024-02-13 20:02:35 -0500

    Update ntds-parse.sh
    lanman updates

--- ntds-parse.sh
+++ ntds-parse.sh
@@ -9,8 +9,13 @@ cat ntds.all | cut -d : -f4 > ntds.nt.hashtopolis
 echo "`cat ntds.nt.hashtopolis | sort -u | wc -l` unique user password hashes"
 
 #LM Hashes enabled
-cat $1 | grep -av 'aad3b435b51404eeaad3b435b51404ee' | cut -d : -f1 > ntds.lm.hash.enabled
-echo "`cat ntds.lm.hash.enabled | wc -l` accounts with LM hashes enabled"
+cat $1 | grep -av 'aad3b435b51404eeaad3b435b51404ee' | cut -d : -f1 > ntds.lm.hash.accounts
+echo "`cat ntds.lm.hash.accounts | wc -l` accounts with LM hashes enabled"
+
+#Add script for creating crackable LM file
+cat ntds.lm.hash.accounts | cut -d : -f3 | fold -w 16 > ntds.lm.hashtopolis
+#john --format=NT -w=lm-dictionary.txt --rules=shifttoggle LM.cracked
+
 
 #Blank Password
 cat ntds.all | grep -a '31d6cfe0d16ae931b73c59d7e0c089c0' | cut -d : -f1 > ntds.no.pass
